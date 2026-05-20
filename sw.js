8e0db0a3f6eddfa2fc2a7c7af71e6a4ca92573e0
lejosaco208032-oss <lejosaco208032@gmail.com>
2026-05-20 04:02:46 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'pwa-ultra-cache-v3';
+const CACHE_NAME = 'pwa-ultra-cache-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './register-sw.js', './icons/icon-192x192.png', './icons/icon-512x512.png'];
 
 self.addEventListener('install', e => {
